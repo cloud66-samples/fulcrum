8252afe22d1a98abf6305bbb7cf22248cef12c5d
Philip Kallberg <philip@cloud66.com>
2013-09-12 17:47:56 +0100

    update

--- .cloud66/scripts/seed.sh
+++ .cloud66/scripts/seed.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /var/.cloud66_env
 cd $RAILS_STACK_PATH
+echo "Fulcrum::Application.config.secret_token = '$(bundle exec rake secret)'" > config/initializers/secret_token.rb
 bundle exec rake db:setup
