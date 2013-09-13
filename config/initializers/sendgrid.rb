b96e5dc4cadcb2a8f1bcfa2272fda3f05c3cb433
Philip Kallberg <philip@cloud66.com>
2013-09-13 12:21:22 +0100

    update

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,8 +1,8 @@
 # Configure Heroku to use SendGrid.
 if ENV['SENDGRID_USERNAME']
   ActionMailer::Base.smtp_settings = {
-    :address        => ENV['SMTP_ADDRESS'],
-    :port           => ENV['SMTP_PORT'],
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
